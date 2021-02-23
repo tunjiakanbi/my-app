64ae3d025f02d0bcb4d0953fce2bee6339639d6a
Tunji Akanbi <you@example.com>
2021-02-23 09:58:08 -0600

    quick edits

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const lakeList = ["Echo Lake", "Maud Lake", "Cascade Lake"]
+
+// function App(props){
+function App({lakes}){
+  return (
+    <ul>
+      {/* {props.lakes.map(lake =>( */}
+      {lakes.map(lake =>(
+        <li>{lake}</li>
+      ))}
+    </ul>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App lakes={lakeList}/>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
